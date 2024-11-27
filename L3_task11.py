16c8bf6d7516c5d999540da14fba7adbdc929bed
ntairova <38103230+ntairova@users.noreply.github.com>
2024-11-27 17:00:21 +0300

    Update L3_task11.py
    function name was updated

--- L3_task11.py
+++ L3_task11.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-def test_new_api():
+def test_cookies():
     response = requests.post("https://playground.learnqa.ru/api/homework_cookie")
     print(dict(response.cookies))
     assert "HomeWork" in response.cookies and "hw_value1" in response.cookies.get('HomeWork'), "invalid or empty cookies"
