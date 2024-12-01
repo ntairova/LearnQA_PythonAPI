d425701d8a5cb338827e3178e47cf6b1176ee753
Nelya <nelya.tairova@gmail.com>
2024-12-01 18:28:52 +0300

    Lesson 3 Task11

--- tests_from_l1_l2_l3/L2_task6.py
+++ tests_from_l1_l2_l3/L2_task6.py
@@ -3,5 +3,6 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 print(len(response.history))
+print(response.history)
 final_response = response
 print(final_response.url)
