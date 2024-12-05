71c979fd547eb1564f7067f40f70b4e265e2bb3d
Nelya <nelya.tairova@gmail.com>
2024-12-05 15:41:29 +0300

    добавлено логирование

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,5 +1,7 @@
 import requests
 
+from lib.logger import Logger
+
 
 class MyRequests():
     @staticmethod
@@ -29,6 +31,8 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
+        Logger.add_request(url, data, headers, cookies, method)
+
         if method == "GET":
             response = requests.get(url, params=data,headers=headers, cookies=cookies)
         elif method == "POST":
@@ -39,6 +43,8 @@ class MyRequests():
             response = requests.delete(url, data=data,headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method '{method}' was received")
+
+        Logger.add_response(response)
 
         return response
 
