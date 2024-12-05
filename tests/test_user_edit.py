502a7a1d0b2599e3e61c2bc6df0fa42354e46b55
Nelya <nelya.tairova@gmail.com>
2024-12-05 12:54:33 +0300

    вынос запроса в метод и замена методов

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,12 +1,14 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
 
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         #Register
         register_data = self.prepare_registration_data()
-        response1 =  requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+        response1 =  MyRequests.post("/user/", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1,"id")
@@ -22,7 +24,7 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("/user/login", data=login_data)
 
 
         auth_sid = self.get_cookie(response2, "auth_sid")
@@ -30,7 +32,7 @@ class TestUserEdit(BaseCase):
 
         #edit
         new_name = "Changed Name"
-        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(f"/user/{user_id}",
                                   headers={"x-csrf-token": token},
                                   cookies={"auth_sid": auth_sid},
                                   data={"firstName": new_name}
@@ -39,7 +41,7 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3, 200)
 
         #get
-        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(f"/user/{user_id}",
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -3,10 +3,12 @@ from requests import Response
 
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
 
 class TestUserGet(BaseCase):
     def test_get_user_details_not_auth(self):
-        response = requests.get("https://playground.learnqa.ru/api/user/2")
+        response = MyRequests.get("/user/2")
         Assertions.assert_json_has_key(response, 'username')
         Assertions.assert_json_has_not_key(response, 'firstName')
         Assertions.assert_json_has_not_key(response, 'lastName')
@@ -15,14 +17,14 @@ class TestUserGet(BaseCase):
     def test_get_user_details_auth_as_same_user(self):
         data = {'email':'vinkotov@example.com',
                'password':'1234'}
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
 
-        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+        response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
@@ -35,7 +37,7 @@ class TestUserGet(BaseCase):
         username_not_auth_user = 'Unauth User Name'
         data = self.prepare_registration_data()
         data['username'] = username_not_auth_user
-        response1 =  requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response1 =  MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1,"id")
@@ -44,13 +46,13 @@ class TestUserGet(BaseCase):
         #login as existing user vinkotov@example.com
         data = {'email':'vinkotov@example.com',
                'password':'1234'}
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response2 = MyRequests.post("/user/login", data=data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
 
         #try to get details for new_created user logged as another user:vinkotov@example.com
-        response3 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_create_user_method}",
+        response3 = MyRequests.get(f"/user/{user_id_from_create_user_method}",
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
